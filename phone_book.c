20094bd84bd855a34a4590b19386e9952605d49f
ITejaswi <84660145+ITejaswi@users.noreply.github.com>
2021-07-08 21:09:11 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -207,9 +207,24 @@ int delete(FILE *db_file, char *name) {
       */
 
       /* TBD */
+       if(strcmp(base->name, name) == 0){
+         base = p->next;
+       }
+      else{ p = p->next;
+      
+       prev->next = p;
+          }
+      
+       deleted = 1;
+       break;
+    }
+    else{
+      prev = p;
+      p = p->next;
     }
   }
   write_all_entries(base);
   free_entries(base);
   return deleted;
 }
+
